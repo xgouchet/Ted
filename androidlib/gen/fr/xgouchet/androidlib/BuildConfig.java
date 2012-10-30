bf6b3c3f63e081d33c70820cfc30e46c6d917db5
Xavier GOUCHET <android@xgouchet.fr>
2012-10-30 12:13:47 +0100

    Cleaning up repository

--- androidlib/gen/fr/xgouchet/androidlib/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package fr.xgouchet.androidlib;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- eclipse-project/gen/fr/xgouchet/texteditor/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package fr.xgouchet.texteditor;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
